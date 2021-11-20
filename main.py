8992d1efdd15e27b43a577119d678861a42c69ad
mattqg <matthew.q.gothard@vanderbilt.edu>
2021-11-20 15:28:26 -0500

    Update main.py
    added todo

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ if __name__ == "__main__":
     button = Button(type = "And", id = 0, name = "AND#0", x = 8, y =-4, width = 10, height = 10, rotation = 0)
     board.add_button(button)
     board.stringify('y')
+
+    # TODO:
+    # give id to each line
+    # loop through lines, finding the word that is closest to each endpoint, add to dict with word key, line id value in list along w/ coords
+    # Classify inputs / outputs by x and y dim for each word
+    # Write_input method to write id, position is based on type of block, need template, set values to zero
+    # FIGURE OUT WIRES
